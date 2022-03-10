3224623eac67041085cd0303a263dfbbd91ab864
Elizar Abramov <nezha2077@mail.ru>
2022-03-10 19:36:26 +0700

    Changes committed

--- src/main/java/ru/netology/domain/TV.java
+++
@@ -1,7 +0,0 @@
-package ru.netology.domain;
-
-public class TV extends Product {
-    public TV(int id, String name, int price) {
-        super(id, name, price);
-    }
-}
--- src/test/java/ru/netology/manager/ProductManagerTest.java
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import ru.netology.domain.Book;
 import ru.netology.domain.Product;
 import ru.netology.domain.Smartphone;
-import ru.netology.domain.TV;
 import ru.netology.repository.Repository;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
@@ -13,20 +12,26 @@ class ProductManagerTest {
     private Repository repository = new Repository();
     private ProductManager productManager = new ProductManager(repository);
     private Book story = new Book(1, "Story", 222, "John");
-    private Book legend = new Book(2, "Legend", 333, "Michael");
+    private Book legend = new Book(2, "g45", 333, "Michael");
     private Smartphone samsong = new Smartphone(3, "g45", 1000, "samsong");
     private Smartphone iponel = new Smartphone(4, "i12", 2000, "iponel");
-    private TV sunny = new TV(1, "sunny", 2500);
 
     @Test
-    public void shouldAddAndSearchBook() {
+    public void shouldAddAndSearchBookByAuthor() {
         productManager.add(story);
-        productManager.searchBy("John");
 
         Product[] actual = productManager.searchBy("John");
         Product[] expected = new Product[]{story};
 
         assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    public void shoulAddAnddSearchBookByName() {
+        productManager.add(story);
+
+        Boolean actual = productManager.matches(story, "Story");
+        Boolean expected = true;
     }
 
     @Test
@@ -37,22 +42,6 @@ class ProductManagerTest {
 
         Product[] actual = repository.findAll();
         Product[] expected = new Product[]{iponel};
-    }
-
-    @Test
-    public void shouldSearchBookByName() {
-        productManager.add(story);
-
-        Boolean actual = productManager.matches(story, "Story");
-        Boolean expected = true;
-    }
-
-    @Test
-    public void shouldNotFindAnyMatches() {
-        productManager.add(sunny);
-
-        Boolean actual = productManager.matches(sunny, "sunny");
-        Boolean expected = false;
     }
 
     @Test
@@ -73,7 +62,7 @@ class ProductManagerTest {
     }
 
     @Test
-    public void shouldNotSearchBookWithWrongText() {
+    public void shouldSearchBookWithWrongText() {
         productManager.add(story);
         Product[] actual = productManager.searchBy("random");
         Product[] expected = new Product[]{};
@@ -81,9 +70,26 @@ class ProductManagerTest {
     }
 
     @Test
-    public void shouldNotSearchSmartpnoneWithWrongText() {
+    public void shouldSearchSmartphoneWithWrongText() {
         productManager.add(iponel);
         Product[] actual = productManager.searchBy("random");
+        Product[] expected = new Product[]{};
+        assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldSearchProductsWithSameRequest() {
+        productManager.add(legend);
+        productManager.add(samsong);
+        Product[] actual = productManager.searchBy("g45");
+        Product[] expected = new Product[]{legend, samsong};
+        assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldSearchProductWithEmptyRequest() {
+        productManager.add(legend);
+        Product[] actual = productManager.searchBy(" ");
         Product[] expected = new Product[]{};
         assertArrayEquals(actual, expected);
     }
